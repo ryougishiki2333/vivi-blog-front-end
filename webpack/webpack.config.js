c32a8f324303c3e58f94f4c5b1d54b7a3b891c51
miaomiaomiao <ryougishiki23333@gmail.com>
2022-11-14 16:44:21 +0800

    完成一部分loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,1 +1,49 @@
-'use strict';   
+"use strict";
+
+const path = request("path");
+
+// 相对路径转绝对路径
+const resolvePath = (_path) => path.resolve(__dirname, _path);
+
+module.exports = {
+  entry: resolvePath("../src/index.tsx"),
+
+  output: {
+    path: resolvePath("../dist"),
+    clean: true,
+    filename: "scripts/[name].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|webp|svg)$/,
+        type: "asset",
+        generator: {
+          filename: "assets/img/[hash:10][ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 60 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.(woff2?|ttf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
