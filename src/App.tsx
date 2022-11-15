94b7d65a9fff170ef74319880dd860a3dd8b57f2
miaomiaomiao <ryougishiki23333@gmail.com>
2022-11-15 09:02:29 +0800

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App: React.FC<any> = () => {
-  return(
-    <div className='App'>
+  return (
+    <div className="App">
       <h2>hello react</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
