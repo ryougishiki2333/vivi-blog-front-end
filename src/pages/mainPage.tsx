cc00d6ee26d7dd6ecebd55e80ee8f51fe80e6fa9
miaomiaomiao <ryougishiki23333@gmail.com>
2022-11-16 11:53:52 +0800

    标题解决

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 const Computer: React.FC = () => {
-  return <div className="App-route-component">I am Computer</div>;
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+
+  return (
+    <Wrapper>
+      <Title>Vivi小站</Title>
+    </Wrapper>
+  );
 };
 
 export default Computer;
