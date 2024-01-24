c20eed80233304dbc887db6f5a18dc400e3d3afe
miaomiaomiao <ryougishiki23333@gmail.com>
2024-01-24 21:30:44 +0800

    feat: 路由

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,37 @@
-# FROM node:16-alpine as builder
+# 第一阶段：使用 Node.js 镜像进行构建
+FROM node:16-alpine as builder
 
-# COPY . .
-
-# RUN npm i
-
-# RUN npm run build 
-
-# COPY ./nginx.conf .
-
-# COPY ./default.conf.template .
-
-FROM nginx:alpine as nginx
-
+# 设置工作目录
 WORKDIR /app
 
-COPY ./dist/ /usr/share/nginx/html/
+# 复制 package.json 和 package-lock.json (如果有)
+COPY package*.json ./
 
+# 安装项目依赖
+RUN npm install
+
+# 复制项目源代码
+COPY . .
+
+# 构建应用程序
+RUN npm run build
+
+# 第二阶段：设置 Nginx
+FROM nginx:alpine
+
+# 设置工作目录
+WORKDIR /usr/share/nginx/html
+
+# 从构建阶段复制构建好的文件到 Nginx 镜像
+COPY --from=builder /app/dist/ .
+
+# 可选：如果需要移除默认的 Nginx 配置，取消以下注释
 # RUN rm /etc/nginx/conf.d/default.conf
 
+# 复制 Nginx 配置文件
 COPY ./nginx.conf /etc/nginx/nginx.conf
+COPY ./default.conf.template /etc/nginx/conf.d/
 
-COPY ./default.conf.template /etc/nginx/conf.d
-
+# 启动命令
 CMD /bin/sh -c "envsubst '80' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;'
+
--- src/request/request.ts
+++ src/request/request.ts
@@ -11,26 +11,29 @@ interface FetchOptions {
   integrity?: string;
 }
 
-export const baseURL = process.env.NODE_ENV === 'production' ? 'http://65.49.195.65:4000/' : '/'
+export const baseURL = "/";
 // export const baseURL = 'http://65.49.195.65:4000/'
 
-const token = window.localStorage.getItem('token')
+const token = window.localStorage.getItem("token");
 
-export const sendRequest = async (url: string, options: FetchOptions = {}): Promise<Response> => {
-  const response = await fetch(baseURL+url, {
-    method: options.method || 'GET',
+export const sendRequest = async (
+  url: string,
+  options: FetchOptions = {}
+): Promise<Response> => {
+  const response = await fetch(baseURL + url, {
+    method: options.method || "GET",
     headers: {
       "Content-Type": "application/json",
       // "token": `Bearer ${token}`
     },
     body: options.body || undefined,
-    mode: options.mode || 'cors',
-    credentials: options.credentials || 'same-origin',
-    cache: options.cache || 'default',
-    redirect: options.redirect || 'follow',
-    referrer: options.referrer || '',
-    referrerPolicy: options.referrerPolicy || 'no-referrer',
-    integrity: options.integrity || ''
+    mode: options.mode || "cors",
+    credentials: options.credentials || "same-origin",
+    cache: options.cache || "default",
+    redirect: options.redirect || "follow",
+    referrer: options.referrer || "",
+    referrerPolicy: options.referrerPolicy || "no-referrer",
+    integrity: options.integrity || "",
   });
 
   if (!response.ok) {
@@ -38,4 +41,4 @@ export const sendRequest = async (url: string, options: FetchOptions = {}): Prom
   }
 
   return response;
-}
+};
